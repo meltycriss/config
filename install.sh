5b37c7fef9f127727a4fe779df47b785add02f01
criss <criss.lin@hotmail.com>
2021-11-28 22:54:40 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-sudo apt-get install tmux=3.0a-2ubuntu0.3
-sudo apt-get install xclip
+
+echo "Install tmux"
+sudo apt-get install -y tmux=3.0a-2ubuntu0.3
+sudo apt-get install -y xclip
 cp ./.tmux.conf ~/.tmux.conf
+
+echo "Install zsh"
+sudo apt-get install -y zsh
+cd /tmp
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chmod +x install.sh
+./install.sh
+sed -i "s/^plugins=(git.*/plugins=(git vi-mode)/g" ~/.zshrc
